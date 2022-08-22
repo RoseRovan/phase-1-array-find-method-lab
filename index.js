9527987924ccd91fc4d571bb98e02d0a21fe1a38
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-08-22 07:15:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [{
+    year: "2015",
+    result: "L"
+},
+{
+    year: "2014",
+    result: "N/A"
+},
+{
+    year: "2013",
+    result: "L"
+},
+
+]
+
+
+
+function superbowlWin(Array) {
+
+const record = Array.find(element => element.result === "W");
+if (typeof record === 'object') {
+    return record.year;
+}
+}
